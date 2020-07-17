a1454bc75b559d5622d394f6d68f067b7e986660
yuta.yamamoto <yyuta1440@gmail.com>
2020-07-17 08:57:46 +0000

    kadai-tasklist-071701

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
+  before_action :set_task, only:[:show, :edit, :update, :destroy]
   def index
     @tasks=Task.all
   end
 
   def show
-    @task=Task.find(params[:id])
   end
 
   def new
@@ -23,12 +23,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task=Task.find(params[:id])
   end
 
   def update
-    @task=Task.find(params[:id])
-    
     if @task.update(task_params)
       flash[:succes] = 'タスクは正常に更新されました'
       redirect_to @task
@@ -39,7 +36,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task=Task.find(params[:id])
     @task.destroy
     
     flash[:succes]= 'タスクを削除しました'
@@ -48,7 +44,12 @@ class TasksController < ApplicationController
   
   private
   #strong parameter
+  
+  def set_task
+    @task=Task.find(params[:id])
+  end
+
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content,:status)
   end
 end
